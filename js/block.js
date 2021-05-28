87c8c01a538c3a9709455bf47c0dc5c852cb175d
wesd724 <wesd723@naver.com>
2021-05-28 23:45:55 +0900

    If there is another block below it, it stops

--- js/block.js
+++ js/block.js
@@ -7,7 +7,7 @@ const blockShape = [
         [1, 0, 0, 0], // 0,0,0
         [1, 0, 0, 0], // 0,1,0
         [1, 0, 0, 0], // 0,2,0
-        [1, 1, 1, 0]  // 0,3,0
+        [1, 1, 1, 0]  // 0,3,0 0,3,1 0,3,2
     ],
     [
         [1, 1, 0], // 1,0,0   1,0,1
@@ -47,6 +47,7 @@ let index = 0;
 console.log(index);
 let fillBlock = 0;
 let dy = 0;
+let dx = 0;
 
 const height = canvas.height / 20;
 const width = canvas.width / 20;
@@ -54,7 +55,66 @@ const blockSize = 20;
 let blockCoordinate = { x: 10, y: 1 };
 
 const downBlock = () => {
-    if(status) {  
+    blockCoordinate.y++;
+    for(let i = 0; i < blockShape[index].length; i++) {
+        for(let j = 0; j < blockShape[index].length; j++) {
+            if(blockShape[index][i][j] == 1) {
+                dy = i;
+                mapCoordinate[blockCoordinate.y - 1 + dy][blockCoordinate.x + j] = 0;
+            }
+        }
+    }
+    for(let i = 0; i < blockShape[index].length; i++) {
+        for(let j = 0; j < blockShape[index].length; j++) {
+            if(blockShape[index][i][j] == 1) {
+                dy = i;
+                mapCoordinate[blockCoordinate.y + dy][blockCoordinate.x + j] = 1;
+            }
+        }
+    }
+    if(blockCoordinate.y + dy == height - 1) {
+        blockCoordinate.y = 1;
+        checkFloor();
+    }
+}
+
+const moveBlock = (e, index) => {
+    fillBlock = 0;
+    if(e.keyCode == 37) {// ←
+        blockCoordinate.x--;
+        for(let i = 0; i < blockShape[index].length; i++) {
+            for(let j = 0; j < blockShape[index].length; j++) {
+                if(blockShape[index][i][j] == 1) {
+                    mapCoordinate[blockCoordinate.y + i][blockCoordinate.x + 1 + j] = 0;
+                }
+            }
+        }
+        for(let i = 0; i < blockShape[index].length; i++) {
+            for(let j = 0; j < blockShape[index].length; j++) {
+                if(blockShape[index][i][j] == 1) {
+                    mapCoordinate[blockCoordinate.y + i][blockCoordinate.x + j] = 1;
+                }
+            }
+        }
+    }
+    else if(e.keyCode == 39) {// →
+        blockCoordinate.x++;
+        for(let i = 0; i < blockShape[index].length; i++) {
+            for(let j = 0; j < blockShape[index].length; j++) {
+                if(blockShape[index][i][j] == 1) {
+                    mapCoordinate[blockCoordinate.y + i][blockCoordinate.x - 1 + j] = 0;
+                }
+            }
+        }
+        for(let i = 0; i < blockShape[index].length; i++) {
+            for(let j = 0; j < blockShape[index].length; j++) {
+                if(blockShape[index][i][j] == 1) {
+                    mapCoordinate[blockCoordinate.y + i][blockCoordinate.x + j] = 1;
+                }
+            }
+        }
+    }
+    else if(e.keyCode == 40) {//↓
         blockCoordinate.y++;
         for(let i = 0; i < blockShape[index].length; i++) {
             for(let j = 0; j < blockShape[index].length; j++) {
@@ -68,83 +128,22 @@ const downBlock = () => {
             for(let j = 0; j < blockShape[index].length; j++) {
                 if(blockShape[index][i][j] == 1) {
                     dy = i;
-                    mapCoordinate[blockCoordinate.y + dy][blockCoordinate.x + j] = 1;
+                    dx = j;
+                    mapCoordinate[blockCoordinate.y + i][blockCoordinate.x + j] = 1;
                 }
             }
         }
+        
         if(blockCoordinate.y + dy == height - 1) {
             blockCoordinate.y = 1;
-            checkFloor();
+            checkFloorFull();
+        } else {
+            checkOtherBlock();
         }
     }
 }
 
-const moveBlock = (e, index) => {
-    if(status) {
-        fillBlock = 0;
-        if(e.keyCode == 37) {// ←
-            blockCoordinate.x--;
-            for(let i = 0; i < blockShape[index].length; i++) {
-                for(let j = 0; j < blockShape[index].length; j++) {
-                    if(blockShape[index][i][j] == 1) {
-                        mapCoordinate[blockCoordinate.y + i][blockCoordinate.x + 1 + j] = 0;
-                    }
-                }
-            }
-            for(let i = 0; i < blockShape[index].length; i++) {
-                for(let j = 0; j < blockShape[index].length; j++) {
-                    if(blockShape[index][i][j] == 1) {
-                        mapCoordinate[blockCoordinate.y + i][blockCoordinate.x + j] = 1;
-                    }
-                }
-            }
-        }
-        else if(e.keyCode == 39) {// →
-            blockCoordinate.x++;
-            for(let i = 0; i < blockShape[index].length; i++) {
-                for(let j = 0; j < blockShape[index].length; j++) {
-                    if(blockShape[index][i][j] == 1) {
-                        mapCoordinate[blockCoordinate.y + i][blockCoordinate.x - 1 + j] = 0;
-                    }
-                }
-            }
-            for(let i = 0; i < blockShape[index].length; i++) {
-                for(let j = 0; j < blockShape[index].length; j++) {
-                    if(blockShape[index][i][j] == 1) {
-                        mapCoordinate[blockCoordinate.y + i][blockCoordinate.x + j] = 1;
-                    }
-                }
-            }
-        }
-        else if(e.keyCode == 40) {//↓
-            blockCoordinate.y++;
-            for(let i = 0; i < blockShape[index].length; i++) {
-                for(let j = 0; j < blockShape[index].length; j++) {
-                    if(blockShape[index][i][j] == 1) {
-                        dy = i;
-                        mapCoordinate[blockCoordinate.y - 1 + dy][blockCoordinate.x + j] = 0;
-                    }
-                }
-            }
-            for(let i = 0; i < blockShape[index].length; i++) {
-                for(let j = 0; j < blockShape[index].length; j++) {
-                    if(blockShape[index][i][j] == 1) {
-                        dy = i;
-                        mapCoordinate[blockCoordinate.y + dy][blockCoordinate.x + j] = 1;
-                    }
-                }
-            }
-            if(blockCoordinate.y + dy == height - 1) {
-                blockCoordinate.y = 1;
-                checkFloor();
-            }
-        }
-    }
-}
-
-
-
-const checkFloor = () => {
+const checkFloorFull = () => {
     for(let i = 0; i < width; i++) {
             if(mapCoordinate[height - 1][i] == 1) {
                 fillBlock++;
@@ -154,6 +153,15 @@ const checkFloor = () => {
                     }
                 }
             }
+    }
+}
+
+const checkOtherBlock = () => {
+    for(let i = 0; i <= dx; i++) {
+        if(mapCoordinate[blockCoordinate.y + dy + 1][blockCoordinate.x + i] == 1) {
+            blockCoordinate.y = 1;
+            break;
+        }
     }
 }
 
