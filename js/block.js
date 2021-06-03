6b556028217a79d8a61bdd22bd03d5e238bc491c
wesd724 <wesd723@naver.com>
2021-06-03 23:47:03 +0900

    work in progress

--- js/block.js
+++ js/block.js
@@ -5,27 +5,28 @@ import { blockShape } from "./blockShape.js";
 
 let fillBlock = 0;
 let dy = 0;
+let rotation = 0;
 let index = Math.floor(Math.random() * 7);
 console.log(index);
 
-const length = blockShape[index].length;
+const length = blockShape[index][rotation][rotation].length;
 const height = canvas.height / 20;
 const width = canvas.width / 20;
 const blockSize = 20;
-const blockCoordinate = { x: 10, y: 0 };
+const blockCoordinate = { x: 10, y: -1 };
 
 const downBlock = () => {
     blockCoordinate.y++;
     for(let i = 0; i < length; i++) {
         for(let j = 0; j < length; j++) {
-            if(blockShape[index][i][j] == 1) {
+            if(blockShape[index][rotation][i][j] == 1) {
                 mapCoordinate[blockCoordinate.y - 1 + i][blockCoordinate.x + j] = 0;
             }
         }
     }
     for(let i = 0; i < length; i++) {
         for(let j = 0; j < length; j++) {
-            if(blockShape[index][i][j] == 1) {
+            if(blockShape[index][rotation][i][j] == 1) {
                 dy = i;
                 mapCoordinate[blockCoordinate.y + i][blockCoordinate.x + j] = 1;
             }
@@ -46,14 +47,15 @@ const moveBlock = (e) => {
         collisionDetection(e.keyCode);
         for(let i = 0; i < length; i++) {
             for(let j = 0; j < length; j++) {
-                if(blockShape[index][i][j] == 1) {
-                    mapCoordinate[blockCoordinate.y + i][blockCoordinate.x + 1 + j] = 0;
+                if(blockShape[index][rotation][i][j] == 1) {
+                    if(blockCoordinate.y > 0) 
+                        mapCoordinate[blockCoordinate.y + i][blockCoordinate.x + 1 + j] = 0;
                 }
             }
         }
         for(let i = 0; i < length; i++) {
             for(let j = 0; j < length; j++) {
-                if(blockShape[index][i][j] == 1) {
+                if(blockShape[index][rotation][i][j] == 1) {
                     mapCoordinate[blockCoordinate.y + i][blockCoordinate.x + j] = 1;
                 }
             }
@@ -63,14 +65,14 @@ const moveBlock = (e) => {
         collisionDetection(e.keyCode);
         for(let i = 0; i < length; i++) {
             for(let j = 0; j < length; j++) {
-                if(blockShape[index][i][j] == 1) {
+                if(blockShape[index][rotation][i][j] == 1) {
                     mapCoordinate[blockCoordinate.y + i][blockCoordinate.x - 1 + j] = 0;
                 }
             }
         }
         for(let i = 0; i < length; i++) {
             for(let j = 0; j < length; j++) {
-                if(blockShape[index][i][j] == 1) {
+                if(blockShape[index][rotation][i][j] == 1) {
                     mapCoordinate[blockCoordinate.y + i][blockCoordinate.x + j] = 1;
                 }
             }
@@ -80,14 +82,15 @@ const moveBlock = (e) => {
         blockCoordinate.y++;
         for(let i = 0; i < length; i++) {
             for(let j = 0; j < length; j++) {
-                if(blockShape[index][i][j] == 1) {
-                    mapCoordinate[blockCoordinate.y - 1 + i][blockCoordinate.x + j] = 0;
+                if(blockShape[index][rotation][i][j] == 1) {
+                  if(blockCoordinate.y > 0)
+                      mapCoordinate[blockCoordinate.y - 1 + i][blockCoordinate.x + j] = 0;
                 }
             }
         }
         for(let i = 0; i < length; i++) {
             for(let j = 0; j < length; j++) {
-                if(blockShape[index][i][j] == 1) {
+                if(blockShape[index][rotation][i][j] == 1) {
                     dy = i;
                     mapCoordinate[blockCoordinate.y + i][blockCoordinate.x + j] = 1;
                 }
@@ -102,15 +105,19 @@ const moveBlock = (e) => {
             collisionDetection(e.keyCode);
         }
     }
+    else if(e.keyCode == 32) {
+        rotationBlock();
+    }
 }
 
 const deleteFilledLine = () => {
+    let lineNumber = 0;
     for(let i = 0; i < height; i++) {
         for(let j = 0; j < width; j++) {
             if(mapCoordinate[i][j] == 1) {
                 fillBlock++;
                 if(fillBlock == width - 1) {
-                    let lineNumber = i;
+                    lineNumber = i;
                     console.log(`DELETE ${lineNumber}`);
                     for(let i = 0; i < width; i++) {
                         mapCoordinate[lineNumber][i] = 0;
@@ -139,11 +146,100 @@ const allLinedown = lineNumber => {
     for(let i = 0; i < width; i++) mapCoordinate[0][i] = 0;
 }
 
+const rotationBlock = () => {
+    for(let i = 0; i < length; i++) {
+        for(let j = 0; j < length; j++) {
+            if(blockShape[index][rotation][i][j] == 1) {
+                  mapCoordinate[blockCoordinate.y + i][blockCoordinate.x + j] = 0;
+            }
+        }
+    } // Delete current block
+
+    
+    if(rotation + 1 > 3) rotation = -1;
+    
+    for(let i = 0; i < length; i++) { //↓
+        for(let j = length - 1; j >= 0; j--) {
+            if(blockShape[index][rotation + 1][j][i] == 1) {
+                if(mapCoordinate[blockCoordinate.y + j][blockCoordinate.x + i] == 1) {
+                    console.log("Impossible Rotation");
+                    if(rotation = - 1) rotation = 0;
+                    for(let i = 0; i < length; i++) {
+                        for(let j = 0; j < length; j++) {
+                            if(blockShape[index][rotation][i][j] == 1) {
+                                dy = i;
+                                mapCoordinate[blockCoordinate.y + i][blockCoordinate.x + j] = 1;
+                            }
+                        }
+                    }
+                    return;
+                } else break;
+            }
+        }
+    }
+    for(let i = 0; i < length; i++) { //←, ↑
+        for(let j = 0; j < length; j++) {
+            if(blockShape[index][rotation + 1][j][i] == 1) {
+                if(mapCoordinate[blockCoordinate.y + j][blockCoordinate.x + i] == 1 ||
+                  blockCoordinate.x + i <= 0) {
+                    console.log("Impossible Rotation");
+                    if(rotation = - 1) rotation = 0;
+                    for(let i = 0; i < length; i++) {
+                        for(let j = 0; j < length; j++) {
+                            if(blockShape[index][rotation][i][j] == 1) {
+                                dy = i;
+                                mapCoordinate[blockCoordinate.y + i][blockCoordinate.x + j] = 1;
+                            }
+                        }
+                    }
+                    return;
+                } else if(mapCoordinate[blockCoordinate.y + j][blockCoordinate.x + i - 1] == 0) {
+                    if(j < length - 1 && blockShape[index][rotation + 1][j + 1][i] == 1) continue;
+                }
+            }
+        }
+    }
+    for(let i = length - 1; i >= 0; i--) { //→
+        for(let j = 0; j < length; j++) {
+            if(blockShape[index][rotation + 1][j][i] == 1) {
+                if(mapCoordinate[blockCoordinate.y + j][blockCoordinate.x + i] == 1 ||
+                  blockCoordinate.x + i >= width - 1) {
+                    console.log("Impossible Rotation");
+                    if(rotation = - 1) rotation = 0;
+                    for(let i = 0; i < length; i++) {
+                        for(let j = 0; j < length; j++) {
+                            if(blockShape[index][rotation][i][j] == 1) {
+                                dy = i;
+                                mapCoordinate[blockCoordinate.y + i][blockCoordinate.x + j] = 1;
+                            }
+                        }
+                    }
+                    return;
+                } else if(mapCoordinate[blockCoordinate.y + j][blockCoordinate.x + i + 1] == 0) {
+                    if(j < length - 1 && blockShape[index][rotation + 1][j + 1][i] == 1) continue;
+                }
+            }
+        }
+    }
+    
+    if(rotation < 3) rotation++;
+    console.log(rotation);
+    
+    for(let i = 0; i < length; i++) {
+        for(let j = 0; j < length; j++) {
+            if(blockShape[index][rotation][i][j] == 1) {
+                dy = i;
+                mapCoordinate[blockCoordinate.y + i][blockCoordinate.x + j] = 1;
+            }
+        }
+    }
+}
+
 const collisionDetection = keyCode => {
     if(keyCode == 40) {
         for(let i = 0; i < length; i++) {
             for(let j = length - 1; j >= 0; j--) {
-                if(blockShape[index][j][i] == 1) {
+                if(blockShape[index][rotation][j][i] == 1) {
                     if(mapCoordinate[blockCoordinate.y + j + 1][blockCoordinate.x + i] == 1) {
                         index = Math.floor(Math.random() * 7);
                         deleteFilledLine();
@@ -156,12 +252,12 @@ const collisionDetection = keyCode => {
     } else if(keyCode == 37) { //←
         for(let i = 0; i < length; i++) {
             for(let j = 0; j < length; j++) {
-                if(blockShape[index][j][i] == 1) {
+                if(blockShape[index][rotation][j][i] == 1) {
                     if(mapCoordinate[blockCoordinate.y + j][blockCoordinate.x + i - 1] == 1 ||
                       blockCoordinate.x + i <= 0) {
                         return blockCoordinate.x;
                     } else if(mapCoordinate[blockCoordinate.y + j][blockCoordinate.x + i - 1] == 0) {
-                        if(j < length - 1 && blockShape[index][j + 1][i] == 1) continue;
+                        if(j < length - 1 && blockShape[index][rotation][j + 1][i] == 1) continue;
                         return blockCoordinate.x--;
                     }
                 }
@@ -170,12 +266,12 @@ const collisionDetection = keyCode => {
     } else if(keyCode == 39) { //→
         for(let i = length - 1; i >= 0; i--) {
             for(let j = 0; j < length; j++) {
-                if(blockShape[index][j][i] == 1) {
+                if(blockShape[index][rotation][j][i] == 1) {
                     if(mapCoordinate[blockCoordinate.y + j][blockCoordinate.x + i + 1] == 1 ||
                       blockCoordinate.x + i >= width - 1) {
                         return blockCoordinate.x;
                     } else if(mapCoordinate[blockCoordinate.y + j][blockCoordinate.x + i + 1] == 0) {
-                        if(j < length - 1 && blockShape[index][j + 1][i] == 1) continue;
+                        if(j < length - 1 && blockShape[index][rotation][j + 1][i] == 1) continue;
                         return blockCoordinate.x++;
                     }
                 }
