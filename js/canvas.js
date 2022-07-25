1f0b2e520b87779cbd906113168b1b55728a452e
wesd724 <wesd725@gmail.com>
2022-07-25 15:09:28 +0900

    Center the canvas on the screen

--- js/canvas.js
+++ js/canvas.js
@@ -2,9 +2,6 @@ const canvas = document.createElement("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 360;
 canvas.height = 420;
-canvas.style.position = "absolute";
-canvas.style.left = "50px";
-canvas.style.top = "50px";
 
 const rect = (x, y, width, height, color) => {
     ctx.fillStyle = color;
